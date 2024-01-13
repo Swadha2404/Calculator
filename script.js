505e5de889a5585e81dd63c19a943ab8549374f5
Swadha2404 <101729961+Swadha2404@users.noreply.github.com>
2024-01-13 21:27:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let display=document.getElementById('display');
+
+function appendCharacter(char){
+    display.value += char;
+}
+function clearDisplay(){
+    display.value= '';
+}
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value=error;
+    }
+}
